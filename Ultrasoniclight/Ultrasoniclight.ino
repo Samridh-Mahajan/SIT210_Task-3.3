9cc9470ff7b289ef77336e74c2171bd7208e99ff
Samridh-Mahajan <133039483+Samridh-Mahajan@users.noreply.github.com>
2023-09-07 01:39:13 +0530

    Ultrasoniclight.ino

--- Ultrasoniclight/Ultrasoniclight.ino
+++ Ultrasoniclight/Ultrasoniclight.ino
@@ -5,7 +5,9 @@
   Name: Samridh Mahajan
   Student Id: 2210994834
 */
-#include <ArduinoMqttClient.h>
+#include <ArduinoMqttClient>
+
+// Include the appropriate Wi-Fi library based on the board being used
 #if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
 #include <WiFiNINA.h>
 #elif defined(ARDUINO_SAMD_MKR1000)
@@ -16,34 +18,34 @@
 #include <WiFi.h>
 #endif
 
-// #include "arduino_secrets.h"
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = "@prabh";    // your network SSID (name)
-char pass[] = "0987654321";    // your network password (use for WPA, or use as key for WEP)
+// Network credentials
+char wifiSSID[] = "@prabh";             // Your network SSID (name)
+char wifiPassword[] = "0987654321";     // Your network password (use for WPA, or use as key for WEP)
 
-int light = 2;
+int ledPin = 2;
 
-
+// MQTT setup
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 
 const char broker[] = "mqtt-dashboard.com";
-int        port     = 1883;
-const char topic[]  = "SIT210/waves";
+int brokerPort = 1883;
+const char mqttTopic[] = "SIT210/waves";
 
 void setup() {
-  //Initialize serial and wait for port to open:
+  // Initialize serial and wait for port to open
   Serial.begin(9600);
-  pinMode(light, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    ; // Wait for serial port to connect (needed for native USB port only)
   }
 
-  // attempt to connect to WiFi network:
+  // Attempt to connect to WiFi network
   Serial.print("Attempting to connect to WPA SSID: ");
-  Serial.println(ssid);
-  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
-    // failed, retry
+  Serial.println(wifiSSID);
+
+  while (WiFi.begin(wifiSSID, wifiPassword) != WL_CONNECTED) {
     Serial.print(".");
     delay(5000);
   }
@@ -51,62 +53,50 @@ void setup() {
   Serial.println("You're connected to the network");
   Serial.println();
 
+  // Attempt to connect to the MQTT broker
   Serial.print("Attempting to connect to the MQTT broker: ");
   Serial.println(broker);
 
-  if (!mqttClient.connect(broker, port)) {
+  if (!mqttClient.connect(broker, brokerPort)) {
     Serial.print("MQTT connection failed! Error code = ");
     Serial.println(mqttClient.connectError());
-
     while (1);
   }
 
   Serial.println("You're connected to the MQTT broker!");
   Serial.println();
 
+  // Subscribe to a topic
   Serial.print("Subscribing to topic: ");
-  Serial.println(topic);
+  Serial.println(mqttTopic);
   Serial.println();
 
-  // subscribe to a topic
-  mqttClient.subscribe(topic);
-
-  // topics can be unsubscribed using:
-  // mqttClient.unsubscribe(topic);
-
-  Serial.print("Waiting for messages on topic: ");
-  Serial.println(topic);
-  Serial.println();
+  mqttClient.subscribe(mqttTopic);
 }
 
 void loop() {
   int messageSize = mqttClient.parseMessage();
   if (messageSize) {
-    // we received a message, print out the topic and contents
     Serial.print("Received a message with topic '");
     Serial.print(mqttClient.messageTopic());
     Serial.print("', length ");
     Serial.print(messageSize);
     Serial.println(" bytes:");
 
-    // use the Stream interface to print the contents
     while (mqttClient.available()) {
       Serial.print((char)mqttClient.read());
     }
+
     Serial.println();
     Serial.println("LED BLINK");
-    digitalWrite(light, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(200);                       // wait for a second
-    digitalWrite(light, LOW);    // turn the LED off by making the voltage LOW
-    delay(200);
-    digitalWrite(light, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(200);                       // wait for a second
-    digitalWrite(light, LOW);    // turn the LED off by making the voltage LOW
-    delay(200);
-    digitalWrite(light, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(200);                       // wait for a second
-    digitalWrite(light, LOW);    // turn the LED off by making the voltage LOW
-    delay(200);
+
+    // Blink the LED
+    for (int i = 0; i < 3; i++) {
+      digitalWrite(ledPin, HIGH);
+      delay(200);
+      digitalWrite(ledPin, LOW);
+      delay(200);
+    }
 
     Serial.println();
   }
